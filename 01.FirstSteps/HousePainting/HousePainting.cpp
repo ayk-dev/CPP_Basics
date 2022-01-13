dcd029ce65710dc59a61fbc02fb8f4a13c4f0857
Albena Krasteva <albena.y.krasteva@gmail.com>
2022-01-13 02:37:31 -0600

    HousePainting.cpp

--- 01.FirstSteps/HousePainting/HousePainting.cpp
+++ 01.FirstSteps/HousePainting/HousePainting.cpp
@@ -1,20 +1,30 @@
-// HousePainting.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double height, sideLength, roofHeight;
+    cin >> height >> sideLength >> roofHeight;
+
+    // green paint
+    double door = 1.2 * 2;
+    double frontAndBackWall = height * height * 2 - door;
+
+    double windows = 1.5 * 1.5 * 2;
+    double sideWalls = height * sideLength * 2 - windows;
+
+    double totalArea = frontAndBackWall + sideWalls;
+    double greenPaint = totalArea / 3.4;
+
+    // red paint
+    double roofArea = (height * roofHeight / 2) * 2 + sideWalls;
+    double redPaint = roofArea / 4.3;
+
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    cout << greenPaint << endl;
+    cout << redPaint << endl;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

3081c5c2a79d6999635473dd5e93a83b360bd0b4
Albena Krasteva <albena.y.krasteva@gmail.com>
2022-01-21 14:10:19 -0600

    Problem fixed

--- ConditionalStatements/LunchBreak/LunchBreak.cpp
+++ ConditionalStatements/LunchBreak/LunchBreak.cpp
@@ -17,16 +17,17 @@ int main()
     lunch = breakTime * 0.125;
     relax = breakTime * 0.25;
 
-    double spareTime = floor(breakTime - lunch - relax);
+    double spareTime = breakTime - lunch - relax;
 
-    int timeLeft = abs(spareTime - episodeTime);
+    double timeLeft = ceil(spareTime - episodeTime);
     if (spareTime >= episodeTime) {
         cout << "You have enough time to watch " << tvShow << 
             " and left with " << timeLeft << " minutes free time." << endl;
     }
     else {
+        double timeShort = ceil(episodeTime - spareTime);
         cout << "You don't have enough time to watch " << tvShow 
-            << ", you need " << timeLeft << " more minutes." << endl;
+            << ", you need " << timeShort << " more minutes." << endl;
     }
 
     return 0;

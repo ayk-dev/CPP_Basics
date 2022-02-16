c6b7f16e066ddfffa94885e7f2f0c114db0b7e72
Albena Krasteva <albena.y.krasteva@gmail.com>
2022-02-16 12:19:30 -0600

    Nested loops Prime Numbers

---
+++ NestedLoops/PrimeNumbers/PrimeNumbers.cpp
@@ -0,0 +1,31 @@
+// PrimeNumbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /* variable definition and initialization */
+    int n, i, c = 0;
+
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            c++;
+        }
+    }
+    if (c == 2)
+    {
+        cout << "n is a Prime number" << endl;
+    }
+    else
+    {
+        cout << "n is not a Prime number" << endl;
+    }
+    return 0;
+}
